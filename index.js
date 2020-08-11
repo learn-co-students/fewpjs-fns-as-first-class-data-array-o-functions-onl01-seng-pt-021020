5d33610c3a9d1b197c34189ecf792d84dbab6843
TerryThreatt <terry.threatt@gmail.com>
2020-08-11 17:02:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Expected output:
- console.log("Wake Byron the poodle");
+console.log("Wake Byron the poodle");
 console.log("Leash Byron the poodle");
 console.log("Walk to the park with Byron the poodle");
 console.log("Throw the frisbee for Byron the poodle");
@@ -7,26 +7,39 @@ console.log("Walk home with Byron the poodle");
 console.log("Unleash Byron the poodle");
 */
 
-function wakeDog(name, dog) {
-    console.log(`Wake ${name} the ${dog}`);
+function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`;
 }
 
-function leashDog(name, dog) {
-    console.log(`Leash ${name} the ${dog}`);
+function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`;
 }
-function walkToPark(name, dog) {
-    console.log(`Walk to the park with ${name} the ${dog}`);
+function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
-function throwFrisbee(name, dog) {
-    console.log(`Throw the frisbee for ${name} the ${dog}`);
+function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
-function walkHome(name, dog) {
-    console.log(`Walk home with ${name} the ${dog}`);
+function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`;
 }
-function unleashDog(name, dog) {
-    console.log(`Unleash ${name} the ${dog}`);
+function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`;
 }
 
-function wakeDog(name, dog) {
-    console.log(`Wake ${name} the ${dog}`)
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(dogName, dogBreed) {
+    let results = [];
+    for (let i = 0; i < routine.length; i++) {
+        let result = routine[i](dogName,dogBreed);
+        results.push(result)
+    }
+    return results
 }
