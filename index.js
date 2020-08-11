fd22022bc2b1a9fa8838f93b6f7290932360e075
TerryThreatt <terry.threatt@gmail.com>
2020-08-11 16:11:11 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+/* Expected output:
+ console.log("Wake Byron the poodle");
+console.log("Leash Byron the poodle");
+console.log("Walk to the park with Byron the poodle");
+console.log("Throw the frisbee for Byron the poodle");
+console.log("Walk home with Byron the poodle");
+console.log("Unleash Byron the poodle");
+*/
+
+function wakeDog(name, dog) {
+    console.log(`Wake ${name} the ${dog}`);
+}
+
+function leashDog(name, dog) {
+    console.log(`Leash ${name} the ${dog}`);
+}
+function walkToPark(name, dog) {
+    console.log(`Walk to the park with ${name} the ${dog}`);
+}
+function throwFrisbee(name, dog) {
+    console.log(`Throw the frisbee for ${name} the ${dog}`);
+}
+function walkHome(name, dog) {
+    console.log(`Walk home with ${name} the ${dog}`);
+}
+function unleashDog(name, dog) {
+    console.log(`Unleash ${name} the ${dog}`);
+}
+
+function wakeDog(name, dog) {
+    console.log(`Wake ${name} the ${dog}`)
+}
